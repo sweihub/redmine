d4ed3e5aef86898b4507ea1b48e39dcf3b279481
Alex Wei <alex.wei@jyquant.com.cn>
2021-01-19 11:16:04 +0000

    open external URL in new window

--- plugins/redmine_open_links_in_new_window/assets/javascripts/redmine_open_links_in_new_window.js
+++ plugins/redmine_open_links_in_new_window/assets/javascripts/redmine_open_links_in_new_window.js
@@ -1,47 +1,53 @@
+function isExternalURL(url) {
+    if (url == null || url == "") 
+        return false;   
+    return new URL(url).host !== (location.host);
+}
+
 (function()
-{
-	/**
-	 * Need to be binded to anchor on call
-	 */
-	var handleAnchor = function()
-	{
-		var pass = true;
+    {
+        /**
+         * Need to be binded to anchor on call
+         */
+        var handleAnchor = function()
+        {
+            var pass = true;
 
-		// Requirements
-		if (-1 != this.className.indexOf('wiki-anchor')) pass = false;
+            // Requirements
+            if (-1 != this.className.indexOf('wiki-anchor')) pass = false;
 
-		if (pass)
-		{
-			this.target = '_blank';
-		}
-	};
+            if (pass && isExternalURL(this.href))
+            {
+                this.target = '_blank';
+            }
+        };
 
-	if (document.observe)
-	{
+        if (document.observe)
+        {
 
-		// redmine uses prototype so use it.
+            // redmine uses prototype so use it.
 
-		document.observe('dom:loaded', function()
-		{
-			var links = $$('div.wiki a, div.attachments a, div.journal ul.details a, a.external');
-			for (var i = 0; i < links.length; i++)
-			{
-				handleAnchor.call(links[i]);
-			}
-		});
-	}
-	else if (window.jQuery)
-	{
+            document.observe('dom:loaded', function()
+                {
+                    var links = $$('div.wiki a, div.attachments a, div.journal ul.details a, a.external');
+                    for (var i = 0; i < links.length; i++)
+                    {
+                        handleAnchor.call(links[i]);
+                    }
+                });
+        }
+        else if (window.jQuery)
+        {
 
-		// redmine uses jQuery so use it.
+            // redmine uses jQuery so use it.
 
-		jQuery(document).ready(function()
-		{
-			jQuery('div.wiki a, div.attachments a, div.journal ul.details a, a.external').each(handleAnchor);
-		});
-	}
-	else
-	{
-		console.error('(redmine_open_links_in_new_window) JS-framework is unknown!');
-	}
-})();
+            jQuery(document).ready(function()
+                {
+                    jQuery('div.wiki a, div.attachments a, div.journal ul.details a, a.external').each(handleAnchor);
+                });
+        }
+        else
+        {
+            console.error('(redmine_open_links_in_new_window) JS-framework is unknown!');
+        }
+    })();
